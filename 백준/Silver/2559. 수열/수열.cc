a16b0012581b8539b57815b995538b371c8312be
Pilgyeong Gong <114580848+pilgyeong@users.noreply.github.com>
2022-11-30 11:38:52 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int n, k, temp, pSum[100001], ret = -1000000;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n >> k;
+    for(int i=1; i<=n; i++){
+        cin >> temp;
+        pSum[i] = pSum[i-1] + temp;
+    }
+    for(int i=k; i<=n; i++){
+        ret = max(ret, pSum[i] - pSum[i-k]);
+    }
+    cout << ret << "\n";
+    
+    return 0;
+}
